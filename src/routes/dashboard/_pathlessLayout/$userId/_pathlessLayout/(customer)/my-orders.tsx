5e8efe6ded6612e11bbce57ce35934a6a333dafa
stanjae <stanykhay29@gmail.com>
2026-03-28 00:41:32 +0100

    feat: implemented crud operations for vendors and updated authentication logic and flow -- updted store types

--- src/routes/dashboard/_pathlessLayout/$userId/_pathlessLayout/(customer)/my-orders.tsx
+++ src/routes/dashboard/_pathlessLayout/$userId/_pathlessLayout/(customer)/my-orders.tsx
@@ -1,11 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { UserType } from '@/lib/types';
 import { requireRole } from '@/lib/utils/helpers/helpers';
 
 export const Route = createFileRoute(
   '/dashboard/_pathlessLayout/$userId/_pathlessLayout/(customer)/my-orders'
 )({
   component: RouteComponent,
-  beforeLoad: () => requireRole('customer'),
+  beforeLoad: () => requireRole(UserType.CUSTOMER),
 });
 
 function RouteComponent() {
