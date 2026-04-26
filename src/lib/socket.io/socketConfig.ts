6baa45cf45a5e527a362555c77d83467749a6e31
stanjae <stanykhay29@gmail.com>
2026-04-26 18:52:18 +0100

    feat: socket-io config update

--- src/lib/socket.io/socketConfig.ts
+++ src/lib/socket.io/socketConfig.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io(import.meta.env.VITE_BACKEND_URL + '/api', {
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
   transports: ['websocket', 'polling'],
   reconnection: true,
   reconnectionAttempts: 10,
